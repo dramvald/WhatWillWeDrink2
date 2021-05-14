bc3a8552a139b490cd271032506f58da870d4b6b
Dramvald <kirillpior@gmail.com>
2021-05-14 20:01:38 +0300

    added a file service.py and transferred all the functionality of the search for a cocktail into it, changed Pipfile and Pipfile.lock

--- alkotinder/drink/views.py
+++ alkotinder/drink/views.py
@@ -1,53 +1,21 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
-import requests
-from queue import PriorityQueue
+from . import service
 
 
-ALCOHOL = 'Alcoholic'
 RANDOM_DRINK_API_URL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
 INGREDIENT = 'strIngredient'
 MEASURE = 'strMeasure'
-
-
-def get_drink_date(RANDOM_DRINK_API_URL):
-    """Делам запрос на сервер по адресу req_url.
-    Преобразуем строку json в объект python типа dict."""
-    data = requests.get(RANDOM_DRINK_API_URL).json()
-    # Так как содержимое ключа drinks имеет тип list,
-    # использую for для того чтобы пройтись по элементам списка,
-    # которые являются словарями, и взять нужные данные.
-    for item in data['drinks']:
-        drink = item['strDrink']
-        instruction = item['strInstructions']
-        drink_img = item['strDrinkThumb']
-        # Делаю проверку на алкогольный или без алкогольный напиток, и если он без алкогольный,
-        # то отправляется снова запрос и проходит по циклу.
-        if ALCOHOL != item['strAlcoholic']:
-            return get_drink_date(RANDOM_DRINK_API_URL)
-        else:
-            return drink, instruction, drink_img, data['drinks'][0]
-
-
-def sort_values(drink, property_name):
-    """Эта функция позволяет автоматически найти и записать данные искомых значений"""
-    pq = PriorityQueue()
-    for key, value in drink.items():
-        if isinstance(key, str) and key.startswith(property_name):
-            if value is not None:
-                # Уменьшаю список убирая элементы имеющие None.
-                pq.put((int(key[-1]), value))
-    return [i[1] for i in pq.queue]
 
 
 def drinks(request):
     """Эта функция нужна для отображения необходимой информации на странице drink.html. В нее поступают данные о
     названии коктеля, инструкции, игридиентах их количестве, и картинке самого котеля. После чего эта вся информация
     попадает на страницу."""
-    drink, instruction, drink_img, data_drinks = get_drink_date(RANDOM_DRINK_API_URL)
-    ingredients = sort_values(data_drinks, INGREDIENT)
-    measures = sort_values(data_drinks, MEASURE)
+    drink, instruction, drink_img, data_drinks = service.get_drink_date(RANDOM_DRINK_API_URL)
+    ingredients = service.sort_values(data_drinks, INGREDIENT)
+    measures = service.sort_values(data_drinks, MEASURE)
     context = {"drink": drink, "instruction": instruction, "drink_img": drink_img, "ingredients": ingredients,
                "measures": measures}
     return render(request, 'drink.html', context=context)
