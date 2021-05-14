adfbe31ce444c00a46ddfe845f26c10819360f5b
Dramvald <kirillpior@gmail.com>
2021-05-14 15:02:01 +0300

    fixed pipenvfile and added comment to views

--- alkotinder/drink/views.py
+++ alkotinder/drink/views.py
@@ -42,6 +42,9 @@ def sort_values(drink, property_name):
 
 
 def drinks(request):
+    """Эта функция нужна для отображения необходимой информации на странице drink.html. В нее поступают данные о
+    названии коктеля, инструкции, игридиентах их количестве, и картинке самого котеля. После чего эта вся информация
+    попадает на страницу."""
     drink, instruction, drink_img, data_drinks = get_drink_date(RANDOM_DRINK_API_URL)
     ingredients = sort_values(data_drinks, INGREDIENT)
     measures = sort_values(data_drinks, MEASURE)
