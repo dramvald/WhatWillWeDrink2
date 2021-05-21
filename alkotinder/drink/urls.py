7d1c2cf3d1272391c9bd6385a53345ffaec66450
Dramvald <kirillpior@gmail.com>
2021-05-21 13:18:27 +0300

    created the favorites.html page, dubbed a link on the main page to the favorites.html page, as well as a link on the favorites.html page, to the main page, and a function in the file views.py

--- alkotinder/drink/urls.py
+++ alkotinder/drink/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.get_random_drink, name="get_random_drink"),
+    path("favorites/", views.favorites, name="favorites"),
 ]
--- alkotinder/drink/views.py
+++ alkotinder/drink/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.core.cache import cache
 
+
 from drink.cocktail_db.api_client import CocktailDBApiClient
 
 
@@ -11,3 +12,7 @@ def get_random_drink(request):
     cache.set("random_drink", random_drink)
 
     return render(request, "get_random_drink.html", context=random_drink)
+
+
+def favorites(request):
+    return render(request, "favorites.html")
