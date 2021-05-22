389d812ec168a2e37d6402796e3ac237b1e3495f
Dramvald <kirillpior@gmail.com>
2021-05-22 21:21:29 +0300

    added database fields to models.py, and made the migration

--- alkotinder/drink/models.py
+++ alkotinder/drink/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Drink(models.Model):
+    name_drink = models.CharField("Name drink", max_length=200)
+    drink_url = models.TextField("Url drink img")
+    instruction = models.TextField("Instruction")
+    ingredients = models.CharField("Ingredients", max_length=300)
+    measures = models.CharField("Measures", max_length=300)
