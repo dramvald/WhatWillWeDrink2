6bfbadd90ea6f3ab0d70dd5ee7ec012fef41d85f
Dramvald <kirillpior@gmail.com>
2021-06-03 18:29:18 +0300

    changed the logo on the pages, and added a transition to the galv page on it when pressed

--- alkotinder/alkotinder/settings.py
+++ alkotinder/alkotinder/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os.path
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -82,7 +81,6 @@ DATABASES = {
         "PASSWORD": "passwordb",
         "HOST": "localhost",
         "PORT": "",
-        
     }
 }
 
