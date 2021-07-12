06b39ab55a141256eb07e2b000d1ae420d0e4e4c
Dramvald <kirillpior@gmail.com>
2021-07-12 14:20:32 +0300

    added links between models, but did not make migrations

--- alkotinder/drink/models.py
+++ alkotinder/drink/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
+from accounts.models import User
+
+
+class FavoriteDrink(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
 
 class Drink(models.Model):
     name_drink = models.CharField("Name drink", max_length=200)
@@ -10,6 +16,8 @@ class Drink(models.Model):
         models.CharField("Ingredients", max_length=300), blank=True
     )
     measures = ArrayField(models.CharField("Measures", max_length=300), blank=True)
+
+    favorite_drink = models.ForeignKey(FavoriteDrink, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name_drink
