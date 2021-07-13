b88965bfe592c01c4d5f9e289043d9afc1cb3b70
Dramvald <kirillpior@gmail.com>
2021-07-13 11:36:47 +0300

    fixed links between models

--- alkotinder/drink/models.py
+++ alkotinder/drink/models.py
@@ -4,10 +4,6 @@ from django.contrib.postgres.fields import ArrayField
 from accounts.models import User
 
 
-class FavoriteDrink(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-
-
 class Drink(models.Model):
     name_drink = models.CharField("Name drink", max_length=200)
     drink_url = models.TextField("Url drink img")
@@ -17,7 +13,10 @@ class Drink(models.Model):
     )
     measures = ArrayField(models.CharField("Measures", max_length=300), blank=True)
 
-    favorite_drink = models.ForeignKey(FavoriteDrink, on_delete=models.CASCADE, null=True)
-
     def __str__(self):
         return self.name_drink
+
+
+class FavoriteDrink(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    drink = models.ForeignKey(Drink, on_delete=models.CASCADE, null=True)
