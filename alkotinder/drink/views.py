e2a592e3185e7584a6c57c3b4440851bb7cb9d39
Dramvald <kirillpior@gmail.com>
2021-08-06 00:52:56 +0300

    improved the code for finding a drink, reduced it, to the favorites.html page

--- alkotinder/drink/views.py
+++ alkotinder/drink/views.py
@@ -26,13 +26,10 @@ def get_random_drink(request):
 def list_favorite_drinks(request):
     """Выводим список сохраненных напитков на отдельную страницу. Также здесь осуществляется поиск
     по имени напитка."""
-    search = request.GET.get('search')                                      # обращаюсь к объекту request, далее к словарю GET, вызываю метод get. в ('search') значение в скобках - ключевое слово.
+    search = request.GET.get('search', '')                                      # обращаюсь к объекту request, далее к словарю GET, вызываю метод get. в ('search') значение в скобках - ключевое слово.
     user = User.objects.get(id=request.user.id)                             # request.user.id атрибут представляет id текущего пользователя
-    if search:
-        favorite_drink_list = user.favoritedrink_set.order_by('-id').filter(drink__name__startswith=search)        # фильтр по названию напитка
-    else:
-        favorite_drink_list = user.favoritedrink_set.order_by('-id')
-
+    
+    favorite_drink_list = user.favoritedrink_set.order_by('-id').filter(drink__name__startswith=search)        # фильтр по названию напитка
     return render(request, "favorites.html", {"favorite_drink_list": favorite_drink_list})
 
 
