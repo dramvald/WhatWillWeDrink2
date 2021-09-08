4fcb960930cea1538081a9191758b4174bbd66d7
dramvald <kirillpior@gmail.com>
2021-09-08 21:18:25 +0300

    fixed login and registration forms, now the form does not pull when an error appears

--- alkotinder/drink/models.py
+++ alkotinder/drink/models.py
@@ -21,3 +21,6 @@ class Drink(models.Model):
 class FavoriteDrink(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     drink = models.ForeignKey(Drink, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.drink.name
